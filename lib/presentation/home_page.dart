d391926e278fdddc61d5c1254b4ebe95800dc7a6
Bill Femmer <bfemmer@gmail.com>
2022-08-22 18:28:06 -0400

    Extracted card widget.

--- lib/presentation/home_page.dart
+++ lib/presentation/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:switchboard/presentation/widgets/media_card.dart';
 
 import '../utility/fa_helper.dart';
 import '../utility/url_helper.dart';
@@ -160,153 +161,40 @@ class HomePage extends StatelessWidget {
                 ),
               ),
             ),
-            Padding(
-              padding:
-                  const EdgeInsets.only(left: 10.0, right: 10.0, bottom: 10.0),
-              child: Card(
-                elevation: 3.0,
-                clipBehavior: Clip.antiAlias,
-                child: Column(
-                  children: [
-                    ListTile(
-                      leading: CircleAvatar(
-                          backgroundColor: Colors.transparent,
-                          child: Image.asset(
-                            'assets/images/resilience.png',
-                          )),
-                      title: const Text('Restricted vs Unrestricted'),
-                      subtitle: Text(
-                        'What' 's the difference?',
-                        style: TextStyle(color: Colors.black.withOpacity(0.6)),
-                      ),
-                    ),
-                    Image.asset('assets/images/teal_ribbon.jpeg'),
-                    const SizedBox(
-                      height: 16.0,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 16.0, right: 16.0),
-                      child: Text(
-                        'Deciding to report a sexual assault is a very personal decision. Restricted and Unrestricted reporting options allow military members, dependents (18+) and DoD Air Force civilians who experienced sexual assault to exercise control over how and when they engage with resources.',
-                        style: TextStyle(color: Colors.black.withOpacity(0.6)),
-                      ),
-                    ),
-                    ButtonBar(
-                      alignment: MainAxisAlignment.start,
-                      children: [
-                        TextButton(
-                          onPressed: () {
-                            UrlHelper.launchBrowser(
-                                'https://www.resilience.af.mil/SAPR/Restricted-or-Unrestricted');
-                            //https://www.resilience.af.mil/SAPR/Restricted-or-Unrestricted/
-                          },
-                          child: const Text(
-                            'Learn more',
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+            const Padding(
+              padding: EdgeInsets.only(left: 10.0, right: 10.0, bottom: 10.0),
+              child: MediaCard(
+                title: 'Restricted vs Unrestricted',
+                subTitle: 'What' 's the difference?',
+                description:
+                    'Deciding to report a sexual assault is a very personal decision. Restricted and unrestricted reporting options allow military members, dependents (18+) and DoD Air Force civilians who experience sexual assault to exercise control over how and when they engage with resources.',
+                imageUrl: 'assets/images/teal_ribbon.jpeg',
+                linkUrl:
+                    'https://www.resilience.af.mil/SAPR/Restricted-or-Unrestricted/',
               ),
             ),
-            Padding(
-              padding:
-                  const EdgeInsets.only(left: 10.0, right: 10.0, bottom: 10.0),
-              child: Card(
-                elevation: 3.0,
-                clipBehavior: Clip.antiAlias,
-                child: Column(
-                  children: [
-                    ListTile(
-                      leading: CircleAvatar(
-                          backgroundColor: Colors.transparent,
-                          child: Image.asset(
-                            'assets/images/resilience.png',
-                          )),
-                      title: const Text('Suicide Warning Signs'),
-                      subtitle: Text(
-                        'Do you know them?',
-                        style: TextStyle(color: Colors.black.withOpacity(0.6)),
-                      ),
-                    ),
-                    Image.asset('assets/images/suicide_signs.jpeg'),
-                    const SizedBox(
-                      height: 16.0,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 16.0, right: 16.0),
-                      child: Text(
-                        'Warning Signs for suicide are different than Risk Factors. They are more observable, and should be responded to immediately. A common theme for Warning Signs is change. This is why friends, relatives, and coworkers are critical in detecting early Warning Signs of distress.',
-                        style: TextStyle(color: Colors.black.withOpacity(0.6)),
-                      ),
-                    ),
-                    ButtonBar(
-                      alignment: MainAxisAlignment.start,
-                      children: [
-                        TextButton(
-                          onPressed: () {
-                            UrlHelper.launchBrowser(
-                                'https://www.resilience.af.mil/suicide-prevention-program/');
-                          },
-                          child: const Text(
-                            'Learn more',
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+            const Padding(
+              padding: EdgeInsets.only(left: 10.0, right: 10.0, bottom: 10.0),
+              child: MediaCard(
+                title: 'Suicide Warning Signs',
+                subTitle: 'Do you know them?',
+                description:
+                    'Warning Signs for suicide are different than Risk Factors. They are more observable, and should be responded to immediately. A common theme for Warning Signs is change. This is why friends, relatives, and coworkers are critical in detecting early Warning Signs of distress.',
+                imageUrl: 'assets/images/suicide_signs.jpeg',
+                linkUrl:
+                    'https://www.resilience.af.mil/suicide-prevention-program/',
               ),
             ),
-            Padding(
-              padding:
-                  const EdgeInsets.only(left: 10.0, right: 10.0, bottom: 10.0),
-              child: Card(
-                elevation: 3.0,
-                clipBehavior: Clip.antiAlias,
-                child: Column(
-                  children: [
-                    ListTile(
-                      leading: CircleAvatar(
-                          backgroundColor: Colors.transparent,
-                          child: Image.asset(
-                            'assets/images/resilience.png',
-                          )),
-                      title: const Text(
-                          'Mental Health Counseling and Security Clearances'),
-                      subtitle: Text(
-                        'Is there a negative impact?',
-                        style: TextStyle(color: Colors.black.withOpacity(0.6)),
-                      ),
-                    ),
-                    Image.asset('assets/images/pentagon.jpeg'),
-                    const SizedBox(
-                      height: 16.0,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 16.0, right: 16.0),
-                      child: Text(
-                        'Seeking mental health services does not affect one’s ability to gain or hold clearance eligibility. Adjudicators regard seeking necessary mental health treatment as a positive step in the security clearance process.',
-                        style: TextStyle(color: Colors.black.withOpacity(0.6)),
-                      ),
-                    ),
-                    ButtonBar(
-                      alignment: MainAxisAlignment.start,
-                      children: [
-                        TextButton(
-                          onPressed: () {
-                            UrlHelper.launchBrowser(
-                                'https://www.dcsa.mil/Portals/91/Documents/pv/DODCAF/resources/DCSA-FactSheet_Mental-Health.pdf');
-                          },
-                          child: const Text(
-                            'Learn more',
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+            const Padding(
+              padding: EdgeInsets.only(left: 10.0, right: 10.0, bottom: 10.0),
+              child: MediaCard(
+                title: 'Mental Health Counseling and Security Clearances',
+                subTitle: 'Is there a negative impact?',
+                description:
+                    'Seeking mental health services does not affect one’s ability to gain or hold clearance eligibility. Adjudicators regard seeking necessary mental health treatment as a positive step in the security clearance process.',
+                imageUrl: 'assets/images/pentagon.jpeg',
+                linkUrl:
+                    'https://www.dcsa.mil/Portals/91/Documents/pv/DODCAF/resources/DCSA-FactSheet_Mental-Health.pdf',
               ),
             ),
           ],
---
+++ lib/presentation/widgets/media_card.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+import '../../utility/url_helper.dart';
+
+class MediaCard extends StatefulWidget {
+  final String title;
+  final String subTitle;
+  final String description;
+  final String imageUrl;
+  final String linkUrl;
+
+  const MediaCard({
+    Key? key,
+    required this.title,
+    required this.subTitle,
+    required this.description,
+    required this.imageUrl,
+    required this.linkUrl,
+  }) : super(key: key);
+
+  @override
+  State<MediaCard> createState() => _MediaCardState();
+}
+
+class _MediaCardState extends State<MediaCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 3.0,
+      clipBehavior: Clip.antiAlias,
+      child: Column(
+        children: [
+          ListTile(
+            leading: CircleAvatar(
+                backgroundColor: Colors.transparent,
+                child: Image.asset(
+                  'assets/images/resilience.png',
+                )),
+            title: Text(widget.title),
+            subtitle: Text(
+              widget.subTitle,
+              style: TextStyle(color: Colors.black.withOpacity(0.6)),
+            ),
+          ),
+          Image.asset(widget.imageUrl),
+          const SizedBox(
+            height: 16.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+            child: Text(
+              widget.description,
+              style: TextStyle(color: Colors.black.withOpacity(0.6)),
+            ),
+          ),
+          ButtonBar(
+            alignment: MainAxisAlignment.start,
+            children: [
+              TextButton(
+                onPressed: () {
+                  UrlHelper.launchBrowser(widget.linkUrl);
+                },
+                child: const Text(
+                  'Learn more',
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
