28152cd2abc2be06aefe8f1a5ea3541f1131957c
Bill Femmer <bfemmer@gmail.com>
2022-09-07 21:25:29 -0400

    Refactored UI.

--- lib/presentation/resource_detail_page.dart
+++ lib/presentation/resource_detail_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:switchboard/presentation/widgets/resource_card.dart';
 
 import '../model/resource.dart';
-import '../utility/url_helper.dart';
 
 class ResourceDetailPage extends StatefulWidget {
   final Resource resource;
@@ -16,65 +16,20 @@ class ResourceDetailPage extends StatefulWidget {
 class ResourceDetailPageState extends State<ResourceDetailPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: DefaultTabController(
-        length: 2,
-        child: NestedScrollView(
-            headerSliverBuilder:
-                (BuildContext context, bool innerBoxIsScrolled) {
-              return <Widget>[
-                SliverAppBar(
-                  expandedHeight: 200.0,
-                  floating: false,
-                  pinned: false,
-                  flexibleSpace: FlexibleSpaceBar(
-                      centerTitle: true,
-                      title: const Text('Resource Details',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16.0,
-                          )),
-                      background: Image.asset(
-                        'assets/images/b52.jpeg',
-                        fit: BoxFit.fill,
-                      )),
-                ),
-              ];
-            },
-            body: SafeArea(
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      widget.resource.name!,
-                      style: const TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.blue),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(widget.resource.description!),
-                  ),
-                  widget.resource.link != null
-                      ? ButtonBar(
-                          alignment: MainAxisAlignment.start,
-                          children: [
-                            TextButton(
-                              onPressed: () {
-                                UrlHelper.launchBrowser(widget.resource.link!);
-                              },
-                              child: Text(widget.resource.link!),
-                            ),
-                          ],
-                        )
-                      : Container(),
-                ],
-              ),
-            )),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(title: const Text('Search Result Details')),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ResourceCard(
+                    resource: widget.resource,
+                  )),
+            ],
+          ),
+        ),
       ),
     );
   }
