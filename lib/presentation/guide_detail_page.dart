afe9e11697178a69efca03d191213bf8bf246a46
Bill Femmer <bfemmer@gmail.com>
2022-09-14 19:28:25 -0400

    New page and card widget.

---
+++ lib/presentation/guide_detail_page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:switchboard/presentation/widgets/info_card.dart';
+
+import '../model/guide.dart';
+
+class GuideDetailPage extends StatefulWidget {
+  const GuideDetailPage({Key? key, required this.guide}) : super(key: key);
+
+  final Guide guide;
+
+  @override
+  State<GuideDetailPage> createState() => _GuideDetailPageState();
+}
+
+class _GuideDetailPageState extends State<GuideDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Quick Guide'),
+        ),
+        body: SingleChildScrollView(
+            child: Column(
+          children: [
+            InfoCard(
+                title: widget.guide.name!,
+                description: widget.guide.description!),
+            if (widget.guide.stress != null)
+              InfoCard(
+                  title: 'Sources of stress include:',
+                  description: widget.guide.stress!),
+            InfoCard(
+                title: 'Signs Wingmen Should Know/Look For',
+                description: widget.guide.signs!),
+            if (widget.guide.deployment != null)
+              InfoCard(
+                  title: 'Deployment', description: widget.guide.deployment!),
+            if (widget.guide.riskFactors != null)
+              InfoCard(
+                  title:
+                      'Recognizing Distress in Individuals with Risk Factors',
+                  description: widget.guide.riskFactors!),
+            if (widget.guide.vigilance != null)
+              InfoCard(
+                  title:
+                      'In addition, wingmen must be vigilant when someone shows: ',
+                  description: widget.guide.vigilance!),
+            InfoCard(
+                title: 'Recommended Wingman Action',
+                description: widget.guide.action!),
+            if (widget.guide.assistance != null)
+              InfoCard(
+                  title:
+                      'Seek immediate assistance from the Mental Health Clinic for any of the following:',
+                  description: widget.guide.assistance!),
+            InfoCard(
+                title: 'Leadership Considerations',
+                description: widget.guide.considerations!),
+          ],
+        )));
+  }
+}
+//if (widget.guide.signs != null)
---
+++ lib/presentation/widgets/info_card.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class InfoCard extends StatefulWidget {
+  final String title;
+  final String description;
+
+  const InfoCard({
+    Key? key,
+    required this.title,
+    required this.description,
+  }) : super(key: key);
+
+  @override
+  State<InfoCard> createState() => _InfoCardState();
+}
+
+class _InfoCardState extends State<InfoCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 3.0,
+      clipBehavior: Clip.antiAlias,
+      child: Column(
+        children: [
+          ListTile(
+            leading: CircleAvatar(
+                backgroundColor: Colors.transparent,
+                child: Image.asset(
+                  'assets/images/resilience.png',
+                )),
+            title: Text(widget.title),
+          ),
+          Container(
+            // color: Colors.blue.shade50,
+            child: Row(
+              children: [
+                Flexible(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      widget.description,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
