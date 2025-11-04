7ea23f9637e0af3d69cef8d60edf90290282078c
Bill Femmer <bfemmer@gmail.com>
2025-11-04 17:31:41 -0500

    Reimplementing map.

--- lib/features/units/presentation/views/map_page.dart
+++ lib/features/units/presentation/views/map_page.dart
@@ -1,57 +1,61 @@
-// import 'package:flutter/material.dart';
-// import 'package:google_maps_flutter/google_maps_flutter.dart';
-// import 'package:switchboard/features/units/data/models/unit.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:switchboard/features/units/data/models/unit.dart';
+import 'package:switchboard/features/units/presentation/viewmodels/unit_viewmodel.dart';
 
-// import '../../../../repository/resource_repository.dart';
-// import '../../../../repository/sqlite/sqlite_resource_repository.dart';
+class MapPage extends StatefulWidget {
+  const MapPage({super.key, required this.viewmodel});
+  final UnitViewModel viewmodel;
+  static String route() => "/map";
 
-// class MapPage extends StatefulWidget {
-//   const MapPage({super.key});
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
 
-//   @override
-//   State<MapPage> createState() => _MapPageState();
-// }
+class _MapPageState extends State<MapPage> {
+  late GoogleMapController mapController;
+  late List<Unit> units;
+  final Map<String, Marker> _markers = {};
 
-// class _MapPageState extends State<MapPage> {
-//   late GoogleMapController mapController;
-//   late List<Unit> units;
-//   final Map<String, Marker> _markers = {};
+  final LatLng _center = const LatLng(39.71069, -95.88367);
 
-//   final LatLng _center = const LatLng(39.71069, -95.88367);
+  @override
+  void initState() {
+    super.initState();
+  }
 
-//   Future<void> _onMapCreated(GoogleMapController controller) async {
-//     mapController = controller;
-//     units = await getUnits();
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    widget.viewmodel.load.execute();
+  }
 
-//     setState(() {
-//       _markers.clear();
-//       for (final unit in units) {
-//         final marker = Marker(
-//           markerId: MarkerId(unit.name!),
-//           position: LatLng(unit.lat!, unit.long!),
-//           infoWindow: InfoWindow(title: unit.name, snippet: unit.base),
-//         );
-//         _markers[unit.name!] = marker;
-//       }
-//     });
-//   }
+  Future<void> _onMapCreated(GoogleMapController controller) async {
+    mapController = controller;
+    units = widget.viewmodel.units;
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: const Text('AFRC Units'), elevation: 2),
-//       body: GoogleMap(
-//         onMapCreated: _onMapCreated,
-//         initialCameraPosition: CameraPosition(target: _center, zoom: 4.0),
-//         markers: _markers.values.toSet(),
-//       ),
-//     );
-//   }
+    setState(() {
+      _markers.clear();
+      for (final unit in units) {
+        final marker = Marker(
+          markerId: MarkerId(unit.name!),
+          position: LatLng(unit.lat!, unit.long!),
+          infoWindow: InfoWindow(title: unit.name, snippet: unit.base),
+        );
+        _markers[unit.name!] = marker;
+      }
+    });
+  }
 
-//   Future<List<Unit>> getUnits() async {
-//     ResourceRepository repository = SqliteResourceRepository();
-
-//     units = await repository.getUnits();
-//     return units;
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('AFRC Units'), elevation: 2),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(target: _center, zoom: 4.0),
+        markers: _markers.values.toSet(),
+      ),
+    );
+  }
+}
